09a7c5d27183a14567d9382e8c843305fbaba422
Julia Evans <julevans93@users.noreply.github.com>
2019-12-05 23:35:01 -0500

    application running, methods working

--- src/main/java/io/zipcoder/crudapp/PersonController.java
+++ src/main/java/io/zipcoder/crudapp/PersonController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class PersonController {
     private PersonService service;
 
@@ -15,27 +15,27 @@ public class PersonController {
         this.service = service;
     }
 
-    @GetMapping("/person")
+    @PostMapping("/people")
     public ResponseEntity<Person> create(@RequestBody Person person){
         return new ResponseEntity<>(service.create(person), HttpStatus.CREATED);
     }
 
-    @GetMapping("/person/{id}")
+    @GetMapping("/people/{id}")
     public ResponseEntity<Person> getPerson(@PathVariable Integer id){
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
 
-    @PutMapping("/person/{id}")
+    @PutMapping("/people/{id}")
     public ResponseEntity<Person> updatePerson(@PathVariable Integer id, @RequestBody Person person){
         return new ResponseEntity<>(service.update(id, person), HttpStatus.OK);
     }
 
-    @GetMapping("/person")
+    @GetMapping("/people")
     public ResponseEntity<Iterable<Person>> getPersonList(){
         return new ResponseEntity<>(service.getPersonList(), HttpStatus.OK);
     }
 
-    @DeleteMapping("/person/{id}")
+    @DeleteMapping("/people/{id}")
     public ResponseEntity<Boolean> deletePerson(@PathVariable Integer id){
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
